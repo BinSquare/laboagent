d813588cc3397f7637c25b5aa0ea9d66e7c62184
BinBin He <hebinsquared@gmail.com>
2024-09-21 16:53:34 -0700

    Feature + Refactor: Use channel design for async executions.

--- go.mod
+++ go.mod
@@ -1,21 +1,24 @@
 module github.com/BinSquare/LaboAgent
 
-go 1.22
+go 1.22.0
 
 toolchain go1.22.7
 
 require (
 	github.com/dblohm7/wingoes v0.0.0-20240820181039-f2b84150679e // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/gen2brain/shm v0.1.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-vgo/robotgo v0.110.3 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/kbinani/screenshot v0.0.0-20240820160931-a8a2c5d0e191 // indirect
 	github.com/lufia/plan9stats v0.0.0-20240819163618-b1d8f4d146e7 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/robotn/xgb v0.10.0 // indirect
 	github.com/robotn/xgbutil v0.10.0 // indirect
@@ -24,6 +27,7 @@ require (
 	github.com/tailscale/win v0.0.0-20240830200727-2d6766fe9b74 // indirect
 	github.com/tklauser/go-sysconf v0.3.14 // indirect
 	github.com/tklauser/numcpus v0.8.0 // indirect
+	github.com/tmc/langchaingo v0.1.12 // indirect
 	github.com/vcaesar/gops v0.40.0 // indirect
 	github.com/vcaesar/imgo v0.40.2 // indirect
 	github.com/vcaesar/keycode v0.10.1 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,53 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
+
+type Message struct {
+	Type string
+	Data string
+}
+
+func agentCtrl(ctx context.Context, mailbox chan Message, c *websocket.Conn) {
+	for {
+		select {
+		case <-ctx.Done():
+			// Exit when context is done
+			fmt.Println("Context cancelled, exiting WebSocket listener...")
+			return
+		case msg := <-mailbox:
+			// Process messages sent via the channel
+			switch msg.Type {
+			case "capture_desktop":
+				err, img := captureDesktop()
+				if err != nil {
+					fmt.Println("Error capturing desktop:", err)
+					continue
+				}
+				err = imgToPNG(img)
+				if err != nil {
+					fmt.Println("Error saving image:", err)
+					continue
+				}
+			case "send_message":
+				// Send message to WebSocket
+				sendMessageWebsocket(msg.Data, c)
+
+				// Optionally read a response
+				_, response, err := c.ReadMessage()
+				if err != nil {
+					fmt.Printf("Read error: %v\n", err)
+					// Optionally handle disconnection and reconnection logic here
+					continue
+				}
+				fmt.Printf("Received from server: %s\n", response)
+			}
+		}
+	}
+
+}
 
 func main() {
 	// Create a context with cancellation
@@ -34,39 +80,22 @@ func main() {
 	ticker := time.NewTicker(3 * time.Second) // Adjust the interval as needed
 	defer ticker.Stop()
 
+	mailbox := make(chan Message)
+
+	// Goroutine to handle any messages to the mailbox
+	go agentCtrl(ctx, mailbox, c)
+
+	// Main loop
 	for {
 		select {
 		case <-ctx.Done():
 			// Context has been cancelled, exit the loop
-			fmt.Println("Context cancelled, exiting...")
+			fmt.Println("Context cancelled, exiting main loop...")
 			return
 		case <-ticker.C:
-			// Periodic tasks
-			err, img := captureDesktop()
-			if err != nil {
-				fmt.Println("Error capturing desktop:", err)
-				continue
-			}
-
-			err = imgToPNG(img)
-			if err != nil {
-				fmt.Println("Error saving image:", err)
-				continue
-			}
-
-			// Send a message to the server
-			message := "Screenshot taken and saved locally."
-			sendMessageWebsocket(message, c)
-
-			// Receive a response from the server
-			_, response, err := c.ReadMessage()
-			if err != nil {
-				fmt.Printf("Read error: %v\n", err)
-				// Optionally handle disconnection here
-				// For example, you might want to attempt reconnection
-				continue
-			}
-			fmt.Printf("Received from server: %s\n", response)
+			// Periodic task: capture a screenshot and send a message
+			mailbox <- Message{Type: "capture_desktop", Data: ""}
+			mailbox <- Message{Type: "send_message", Data: "Screenshot taken and saved locally."}
 		}
 	}
 }
